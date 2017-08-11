002e9c8732afc6465aef5d8ec75428c4620b2abd
awefight <547780662@qq.com>
2017-08-11 19:03:53 +0800

    delete

--- 微信二维码收款语音通知/inform_by_voice2.py
+++ 微信二维码收款语音通知/inform_by_voice2.py
@@ -13,32 +13,14 @@ def note():
     @itchat.msg_register(SHARING)
     def inform(msg):
         text=msg['Text']
-        text = "老板老板微信" + text
+        text = "微信" + text
         speak(text=text)
         print(text)
 
-    @itchat.msg_register(TEXT)
-    def test(msg):
-        print(TEXT)
-        speak("老板老板我在线着呢")
 
-    @itchat.msg_register(NOTE)
-    def note(msg):
-        print(msg)
 
-    @itchat.msg_register(SYSTEM)
-    def system(msg):
-        print(msg)
 
-    # @itchat.msg_register()
-    # def s
 if __name__ == '__main__':
     note()
     itchat.auto_login(hotReload=False)
     itchat.run()
-# # 获取特定UserName的公众号，返回值为一个字典
-# itchat.search_mps(userName='@abcdefg1234567')
-# # 获取名字中含有特定字符的公众号，返回值为一个字典的列表
-# itchat.search_mps(name='LittleCoder')
-# # 以下方法相当于仅特定了UserName
-# itchat.search_mps(userName='@abcdefg1234567', name='LittleCoder')
